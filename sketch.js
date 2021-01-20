ca97cf54dc425b8f4d2b8c1714c3edaf6917cea0
Kanak2911 <75474908+Kanak2911@users.noreply.github.com>
2021-01-20 14:35:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,3 +1,66 @@
+// var starImg, fairyImg, bgImg;
+// var fairy , fairyVoice;
+// var star, starBody;
+
+// const Engine = Matter.Engine;
+// const World = Matter.World;
+// const Bodies = Matter.Bodies;
+// const Body = Matter.Body;
+
+// function preload(){
+// 	starImg = loadImage("images/star.png");
+// 	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+// 	bgImg = loadImage("images/starNight.png");
+// 	fairyVoice = loadSound("sound/JoyMusic.mp3");
+// }
+
+// function setup() {
+// 	createCanvas(800, 750);
+	
+// 	fairyVoice.play();
+// 	fairy = createSprite(130, 520);
+// 	fairy.addAnimation("fairyflying",fairyImg);  
+// 	fairy.scale =0.25;
+
+// 	star = createSprite(650,30);
+// 	star.addImage(starImg);
+// 	star.scale = 0.2;
+
+// 	engine = Engine.create();
+// 	world = engine.world;
+
+// 	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+// 	World.add(world, starBody);
+	
+// 	Engine.run(engine);
+
+// 	fairy.setCollider('rectangle',0,0,1000,1000);
+// }
+
+// function draw() {
+//   background(bgImg);
+
+//   keyPressed();
+
+//   drawSprites();
+// }
+
+// function keyPressed() {
+//   if(keyDown('left')){
+// 	 fairy.velocityX = -1;	
+//   }
+//   if(keyDown('right')){
+// 	 fairy.velocityX = 1;	
+//   }
+//   if(keyDown('down')){
+// 	 star.velocityY = 3;	
+//   }
+//   if(fairy.isTouching(star)){
+// 	  star.velocityY = 0;
+// 	  fairy.velocityX = 0;
+//   }
+// }
+
 var starImg, fairyImg, bgImg;
 var fairy , fairyVoice;
 var star, starBody;
@@ -16,8 +79,9 @@ function preload(){
 
 function setup() {
 	createCanvas(800, 750);
-	
+
 	fairyVoice.play();
+
 	fairy = createSprite(130, 520);
 	fairy.addAnimation("fairyflying",fairyImg);  
 	fairy.scale =0.25;
@@ -33,30 +97,31 @@ function setup() {
 	World.add(world, starBody);
 	
 	Engine.run(engine);
-
-	fairy.setCollider('rectangle',0,0,1000,1000);
 }
 
 function draw() {
   background(bgImg);
 
-  keyPressed();
+  star.x= starBody.position.x 
+  star.y= starBody.position.y 
+
+  console.log(star.y);
+
+  if(star.y > 470 && starBody.position.y > 470 ){
+     Matter.Body.setStatic(starBody,true);
+  }
 
   drawSprites();
 }
 
 function keyPressed() {
-  if(keyDown('left')){
-	 fairy.velocityX = -1;	
-  }
-  if(keyDown('right')){
-	 fairy.velocityX = 1;	
-  }
-  if(keyDown('down')){
-	 star.velocityY = 3;	
-  }
-  if(fairy.isTouching(star)){
-	  star.velocityY = 0;
-	  fairy.velocityX = 0;
-  }
+	if(keyCode === RIGHT_ARROW){
+       fairy.x = fairy.x + 20;
+	}
+	if(keyCode === LEFT_ARROW){
+       fairy.x = fairy.x - 20;
+	}
+	if(keyCode === DOWN_ARROW) {
+	   Matter.Body.setStatic(starBody,false); 
+	}
 }
